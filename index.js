8dd6881ed4f14788772a0e168c0cb7519ca81b30
Suheyl Turac <skbsturac@live.com>
2018-09-01 02:18:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 function johnLennonFacts(facts) {
-  const facts = [
+  var facts = [
   "He was the last Beatle to learn to drive", 
   "He was never a vegetarian", 
   "He was a choir boy and boy scout", 
