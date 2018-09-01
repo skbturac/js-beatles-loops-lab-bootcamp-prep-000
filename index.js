f3a424d59d0102472e7ee9b7c3473533a922f44a
Suheyl Turac <skbsturac@live.com>
2018-09-01 02:18:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return array;
 }
-function johnLennonFacts(facts) {
+function johnLennonFacts() {
   var facts = [
   "He was the last Beatle to learn to drive", 
   "He was never a vegetarian", 
